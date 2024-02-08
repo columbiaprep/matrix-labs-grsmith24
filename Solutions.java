3affea34216240232463e480a9fcb802626af0a3
grsmith24 <gsmith24@cgps.org>
2024-02-08 14:08:23 -0500

    Part one

--- Solutions.java
+++ Solutions.java
@@ -9,4 +9,25 @@ public class Solutions {
         return arr;
     }
 
+    public static int[][] multiply(int[][] input1,int[][] input2){
+        int[][] arr= new int[input1.length][input2[0].length];
+        for(int a = 0; a<arr.length; a++) {
+            for (int b = 0; b < arr[0].length; b++) {
+                arr[a][b] = input1[a][b]* input1[b][a];
+            }
+        }
+        return arr;
+    }
+
+    public static int twoXTwoDeterminate(int[][] input){
+        int[][] arr= new int[2][2];
+        int det = arr[0][0]*arr[1][1]-arr[1][0]*arr[0][1];
+        return det;
+    }
+
+    public static int ThreeXThreeDeterminate(int[][] input){
+        int[][] arr= new int[3][3];
+        int det = arr[0][0]*(arr[1][1]*arr[2][2]-arr[1][2]*arr[2][1])+arr[0][1]*(arr[1][0]*arr[2][2]-arr[1][2]*arr[2][0])+arr[0][2]*(arr[1][0]*arr[2][1]-arr[1][1]*arr[2][0]);
+        return det;
+    }
 }
