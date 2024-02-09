cc2d1a22001cd590d067142396f24a06270c8084
grsmith24 <gsmith24@cgps.org>
2024-02-09 15:00:33 -0500

    Final

--- Solutions.java
+++ Solutions.java
@@ -4,6 +4,7 @@ public class Solutions {
         for(int r = 0; r<arr.length; r++) {
             for (int c = 0; c < arr.length; c++) {
                 arr[r][c] = factor * input[r][c];
+                // loops through everything multiply by factor
             }
         }
         return arr;
@@ -13,21 +14,40 @@ public class Solutions {
         int[][] arr= new int[input1.length][input2[0].length];
         for(int a = 0; a<arr.length; a++) {
             for (int b = 0; b < arr[0].length; b++) {
-                arr[a][b] = input1[a][b]* input1[b][a];
+                for (int c=0; c<arr[0].length; c++) {
+                    int sum=0;
+                    sum+= input1[a][c] * input1[c][a];
+                    arr[a][b] = sum;
+                    //multiply item in (row, column) by thing in (column, row)
+                    // the flipping [b][a] sort of transposes the second matrix to access the values with for loops
+                }
             }
         }
         return arr;
     }
 
-    public static int twoXTwoDeterminate(int[][] input){
-        int[][] arr= new int[2][2];
-        int det = arr[0][0]*arr[1][1]-arr[1][0]*arr[0][1];
-        return det;
+    public static int twoXTwoDeterminate(int[][] arr){
+        if(arr.length == arr[0].length) {
+            int det = arr[0][0] * arr[1][1] - arr[1][0] * arr[0][1];
+            //ad-bc
+            return det;
+        }
+        else {
+            System.out.println("Invalid Array");
+            return 0;
+        }
+
     }
 
-    public static int ThreeXThreeDeterminate(int[][] input){
-        int[][] arr= new int[3][3];
-        int det = arr[0][0]*(arr[1][1]*arr[2][2]-arr[1][2]*arr[2][1])+arr[0][1]*(arr[1][0]*arr[2][2]-arr[1][2]*arr[2][0])+arr[0][2]*(arr[1][0]*arr[2][1]-arr[1][1]*arr[2][0]);
-        return det;
+    public static int ThreeXThreeDeterminate(int[][] arr){
+        if(arr.length == arr[0].length) {
+            int det = arr[0][0]*(arr[1][1]*arr[2][2]-arr[1][2]*arr[2][1])+arr[0][1]*(arr[1][0]*arr[2][2]-arr[1][2]*arr[2][0])+arr[0][2]*(arr[1][0]*arr[2][1]-arr[1][1]*arr[2][0]);
+            //3x3 determinate
+            return det;
+        }
+        else {
+            System.out.println("Invalid Array");
+            return 0;
+        }
     }
 }
